6bcd01098e17a89c70b946f95e0fbde985b09dbb
zhulym <zhulym@mail.ru>
2021-07-12 15:06:00 +0300

    feat: start func

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    // if (expr.length % 2 === 0) throw new RangeError("ExpressionError: Brackets must be paired");
+    // if (countExprInBrackets(expr) === Infinity) throw new RangeError("TypeError: Division by zero.");
+
+    function countExprInBrackets(str) {
+        return Function(`'use strict'; return (${str})`)()
+    }
+
+    return countExprInBrackets(expr);
+
 }
 
 module.exports = {
