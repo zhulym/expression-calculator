f0a5e6d223fa29624f644b94fc49fd649aec9de5
zhulym <zhulym@mail.ru>
2021-08-13 17:17:09 +0300

    fix: fix all bugs test 73/73

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 function expressionCalculator(expr) {
+
     let result = expr.split(' ').join('');
     let bracketsIdx = [];
+    bracketsIdx = result.split('').filter(el => el === '(' || el === ')')
+
+    if (bracketsIdx.length % 2 !== 0
+        || bracketsIdx.length === 1
+        || bracketsIdx[0] === ')'
+        || !bracketsIdx.includes(')') && bracketsIdx.length !== 0
+        || !bracketsIdx.includes('(') && bracketsIdx.length !== 0) {
+        throw "ExpressionError: Brackets must be paired";
+    }
 
     while (result.length > 0) {
-        bracketsIdx = result.split('').filter(el => el === '(' || el === ')')
-        if (bracketsIdx.length % 2 !== 0 || bracketsIdx.length === 1) throw "ExpressionError: Brackets must be paired";
         bracketsIdx = [];
         findBracketsIdx(result);
 
@@ -64,33 +72,66 @@ function expressionCalculator(expr) {
             arrNum.splice(operatorPosition, 0, operators[i]);   // вставляем операторы в выражение каждый след оператор будет в позиции i+i+1     0-1  1-3  2-5  3-7
         }
         // ["36", "-", "3", "*", "2", "/", "6", "+", "5"] 
+
+        arrNum = arrNum.filter(el => el !== '');         // убрать возможные пустые строки в массиве (появляются когда промежуточный результат отрицательный)
+
         for (let i = 1; i < arrNum.length; i++) {
+            if (arrNum[0] === '-') {
+                arrNum.splice(0, 2, -(arrNum[1]));
+            }
+
             if (arrNum[i] === '*') {
-                let result1 = (arrNum[i - 1] * arrNum[i + 1]);
-                arrNum.splice(i - 1, 3, result1);
+                if (arrNum[i + 1] === '-') {
+                    let result1 = -(arrNum[i - 1] * arrNum[i + 2]);
+                    arrNum.splice(i - 1, 4, result1);
+                } else {
+                    let result1 = (arrNum[i - 1] * arrNum[i + 1]);
+                    arrNum.splice(i - 1, 3, result1);
+                }
                 i = 0;
             }
+
             if (arrNum[i] === '/') {
-                let result1 = (arrNum[i - 1] / arrNum[i + 1]);
-                arrNum.splice(i - 1, 3, result1);
+                if (arrNum[i + 1] === '-') {
+                    let result1 = -(arrNum[i - 1] / arrNum[i + 2]);
+                    if (result1 === Infinity) throw "TypeError: Division by zero.";
+                    arrNum.splice(i - 1, 4, result1);
+                } else {
+                    let result1 = (arrNum[i - 1] / arrNum[i + 1]);
+                    if (result1 === Infinity) throw "TypeError: Division by zero.";
+                    arrNum.splice(i - 1, 3, result1);
+                }
                 i = 0;
             }
         }
 
         for (let i = 1; i < arrNum.length; i++) {
+            if (arrNum[0] === '-') {
+                arrNum.splice(0, 2, -(arrNum[1]));
+            }
+
             if (arrNum[i] === '+') {
-                let result1 = +(arrNum[i - 1]) + +(arrNum[i + 1]);
-                arrNum.splice(i - 1, 3, result1);
+                if (arrNum[i + 1] === '-') {
+                    let result1 = (arrNum[i - 1] - arrNum[i + 2]);
+                    arrNum.splice(i - 1, 4, result1);
+                } else {
+                    let result1 = +(arrNum[i - 1]) + +(arrNum[i + 1]);
+                    arrNum.splice(i - 1, 3, result1);
+                }
                 i = 0;
             }
 
             if (arrNum[i] === '-') {
-                let result1 = arrNum[i - 1] - arrNum[i + 1];
-                arrNum.splice(i - 1, 3, result1);
+                if (arrNum[i + 1] === '-') {
+                    let result1 = +(+(arrNum[i - 1]) + +(arrNum[i + 2]));
+                    arrNum.splice(i - 1, 4, result1);
+                } else {
+                    let result1 = arrNum[i - 1] - arrNum[i + 1];
+                    arrNum.splice(i - 1, 3, result1);
+                }
                 i = 0;
             }
         }
-
         return Number(arrNum.toString());
     }
     return result.toFixed(4);
